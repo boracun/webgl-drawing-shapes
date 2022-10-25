63a814c1cff3adc01c336e9a7c0653a34afae952
Melisa Tanrıkulu <melisa.tanrikulu@ug.bilkent.edu.tr>
2022-10-25 10:29:21 +0300

    Zoom
    I have fixed the zoom ability, only moving the screen is unsolved

--- homework.js
+++ homework.js
@@ -45,11 +45,11 @@ function getClickPosition(event) {
 	let xComponent = 2 * event.clientX / canvas.width - 1;
 	let yComponent = 2 * (canvas.height - event.clientY) / canvas.height - 1;
 
-	xComponent /= scaleAmount[0];
-	yComponent /= scaleAmount[1];
+	xComponent *= scaleAmount[0];
+	yComponent *= scaleAmount[1];
 
-	xComponent -= translationAmount[0] / scaleAmount[0];
-	yComponent -= translationAmount[1] / scaleAmount[1];
+	xComponent += translationAmount[0];
+	yComponent += translationAmount[1];
 
 	let result = vec2(xComponent, yComponent);
 
@@ -362,15 +362,17 @@ function translateSpace(event) {
 	let positionDiff = subtract(position2, clickPosition);
 
 	let addition = vec3(positionDiff[0] * scaleAmount[0], positionDiff[1] * scaleAmount[1], 0);
-
+	console.log("translationAmount before: ", translationAmount);
+	console.log("addition: ", addition);
 	translationAmount = add(translationAmount, addition);
-	
+	console.log("translationAmount after: ", translationAmount);
 	render();
 }
 
 function zoom(event)
 {
 	zoomPosition = getClickPosition(event);
+	console.log("zoomPosition: ", zoomPosition);
 	
 	render();
 	
@@ -381,7 +383,7 @@ function getTransformationMatrix()
 {
 	let scaleMatrix = scale(scaleAmount[0], scaleAmount[1], 0);
 	//let translationMatrix = translate(translationAmount[0], translationAmount[1], 0);
-	
+
 	if (zoomIn !== null)
 	{
 		//let originPlacement = translate(vec3(-translationAmount[0], -translationAmount[1], 0));
@@ -389,8 +391,11 @@ function getTransformationMatrix()
 		let originScale = scale(1 / scaleAmount[0], 1 / scaleAmount[1], 0);
 		console.log("originScale: ", originScale);
 		
-		scaleAmount = zoomIn ? scale2(SCALE_CONSTANT, scaleAmount) : scale2(1 / SCALE_CONSTANT, scaleAmount);
-		//translationAmount = add(translationAmount, vec3(zoomPosition[0], zoomPosition[1], 0));
+		if (zoomIn !== null)
+			scaleAmount = zoomIn ? scale2(SCALE_CONSTANT, scaleAmount) : scale2(1 / SCALE_CONSTANT, scaleAmount);
+		
+		//if (zoomPosition != null)
+		translationAmount = subtract(translationAmount, vec3(zoomPosition[0], zoomPosition[1], 0));
 		console.log("scaleAmount: ", scaleAmount);
 		//console.log("translationAmount: ", translationAmount);
 		
@@ -399,19 +404,15 @@ function getTransformationMatrix()
 		console.log("scaleMatrix: ", scaleMatrix);
 		//console.log("translationMatrix: ", translationMatrix);
 		
-		//transformationMatrix = mult(translationMatrix, transformationMatrix);
 		transformationMatrix = mult(scaleMatrix, transformationMatrix);
+		//transformationMatrix = mult(translationMatrix, transformationMatrix);
 		transformationMatrix = mult(originScale, transformationMatrix);
 		//transformationMatrix = mult(originPlacement, transformationMatrix);	
 	}
-	
-	else
-	{
-		transformationMatrix = mult(scaleMatrix, transformationMatrix);
-		//transformationMatrix = mult(translationMatrix, transformationMatrix);
-	}
 
 	console.log(transformationMatrix);
+	zoomIn = null;
+	zoomPosition = null;
 	return transformationMatrix;
 }
 
